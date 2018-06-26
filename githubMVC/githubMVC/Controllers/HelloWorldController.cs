3f5bad925e89339fc3d589d3b98ecf13995a9f8c
maplestorykidd <34246540+maplestorykidd@users.noreply.github.com>
2018-06-26 15:43:09 +0800

    添加路由&&修改参数

--- githubMVC/githubMVC/Controllers/HelloWorldController.cs
+++ githubMVC/githubMVC/Controllers/HelloWorldController.cs
@@ -13,9 +13,9 @@ namespace githubMVC.Controllers
         {
             return "This is my <b>default</b> action...";
         }
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
         }
     }
 }
