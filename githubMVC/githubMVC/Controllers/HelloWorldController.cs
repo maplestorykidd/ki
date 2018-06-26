79c7c2ac62e9e22fe52ead64f659931ed10fca2b
maplestorykidd <34246540+maplestorykidd@users.noreply.github.com>
2018-06-26 16:23:54 +0800

    添加一个Helloworld视图

--- githubMVC/githubMVC/Controllers/HelloWorldController.cs
+++ githubMVC/githubMVC/Controllers/HelloWorldController.cs
@@ -9,13 +9,17 @@ namespace githubMVC.Controllers
     public class HelloWorldController : Controller
     {
         // GET: HelloWorld
-        public string Index()
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        //}
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
-        }
-        public string Welcome(string name, int ID = 1)
-        {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            return View();
         }
     }
 }
