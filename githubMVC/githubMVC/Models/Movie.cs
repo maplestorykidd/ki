ae91dd00575eb213bc2371dfe3c6dbaaa978ccf1
maplestorykidd <34246540+maplestorykidd@users.noreply.github.com>
2018-06-27 18:41:42 +0800

    继上

--- githubMVC/githubMVC/Models/Movie.cs
+++ githubMVC/githubMVC/Models/Movie.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data.Entity;
 
 namespace githubMVC.Models
 {
     public class Movie
     {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
     }
 }
