82f163c60fbf93deb2ea3e66c39a68b0523ce84a
maplestorykidd <34246540+maplestorykidd@users.noreply.github.com>
2018-07-03 22:10:54 +0800

    添加搜索功能。标题搜索以及类型搜索

--- githubMVC/githubMVC/Controllers/MoviesController.cs
+++ githubMVC/githubMVC/Controllers/MoviesController.cs
@@ -15,8 +15,14 @@ namespace githubMVC.Controllers
         private MovieDBContext db = new MovieDBContext();
 
         // GET: Movies
-        public ActionResult Index(String searchString)
+        public ActionResult Index(String movieGenre,String searchString)
         {
+            var GenreLst = new List<String>();
+            var GenreQry = from d in db.Movies
+                           orderby d.Genre
+                           select d.Genre;
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreLst);
             //string searchString = id;
             var movies = from m in db.Movies
             select m;
@@ -25,6 +31,10 @@ namespace githubMVC.Controllers
             if (!String.IsNullOrEmpty(searchString))
             {
                 movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
             }
             return View(movies);
         }
